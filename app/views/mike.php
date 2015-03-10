c12d590ad0eaca5a133f8e7630826f74665960a1
diaoyizi <diao.yizi@immomo.com>
2015-03-10 17:48:05 +0800

    a

--- app/views/mike.php
+++ app/views/mike.php
@@ -19,7 +19,9 @@
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
+		<![endif]-->
+
+        <script src="http://s.xnimg.cn/a73263/allunivlist.js"></script>
       </head>
       <body id="top" class="home">
         <header id="home">
@@ -61,3 +63,6 @@
         </header>        
 </body>
 </html>
+<script>
+console.log(allUnivList);
+</script>
